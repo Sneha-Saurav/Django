62f0bacd55759065579f9ea5da8846dae6343da6
Sneha-Saurav <sneha.saurav@netsolutions.com>
2023-03-02 15:48:43 +0530

    Change in django project

--- Django_project/UserApi/views.py
+++ Django_project/UserApi/views.py
@@ -22,7 +22,7 @@ def division(num1,num2):
 
 def index(request):
     form  = """
-    <form method="POST">
+    <form method="GET">
     <input type='text' name='num1'>
     <input type='text' name='num2'>
     <button name='add'>+</button>
@@ -31,23 +31,25 @@ def index(request):
     <button name='div'>/</button>
     </form>
     """
-    result = None
-    if request.method == 'POST':
-        num1 = request.POST['num1']
-        num2 = request.POST['num2']
-        print(request.POST)
+    result = ""
+    if request.method == 'GET':
+        num1 = request.GET.get('num1')
+        num2 = request.GET.get('num2')
+        print(request.GET)
         # if 'Submit' in request.POST:
-        if 'add' in request.POST:
+        if 'add' in request.GET:
             result = Add(num1 , num2)
-        elif 'sub'in request.POST:
+        elif 'sub'in request.GET:
             result = Subtract(num1, num2)
-        elif 'mul' in request.POST:
+        elif 'mul' in request.GET:
             result= Multiply(num1, num2)
-        elif 'div' in request.POST:
+        elif 'div' in request.GET:
             result= division(num1, num2)
         else:
-            result= "Error"
+            result = ""
     return HttpResponse(f"<html>{form} <h2>Result: {result}</h2><html>")
+
+
 
 
 
